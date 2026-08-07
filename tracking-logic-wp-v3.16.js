2c6f8592d9cafa2c3f49c0c80b44e684f023c550
Pro-Leads <info@pro-leads.de>
2026-08-07 14:52:44 +0200

    Update and rename tracking-logic-wp-v3.15.js to tracking-logic-wp-v3.16.js

--- tracking-logic-wp-v3.16.js
+++ tracking-logic-wp-v3.16.js
@@ -1,4 +1,3 @@
-<script>
 // --- V5.3.2_EXTERNAL_THUB_SMART_ROUTING_MASTER ---
 function bootTrackingHub() {
     if (window.thub_initialized) return;
@@ -574,4 +573,3 @@ if (document.readyState === "complete" || document.readyState === "interactive")
     document.addEventListener("DOMContentLoaded", bootTrackingHub);
     window.addEventListener("load", bootTrackingHub);
 }
-</script>
