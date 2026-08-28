3e01e5505390161a06b8d3dcf9cb4e37b43f5cfa
Pro-Leads <info@pro-leads.de>
2026-08-28 12:29:52 +0200

    Create config-calendly-iframe.js

---
+++ config-calendly-iframe.js
@@ -0,0 +1,15 @@
+<div id="calendly-iframe-leadid"></div>
+
+<script>
+    window.calendlyIframeConfig = {
+        baseUrl: "https://calendly.com/va-business-academy/kennlerngespraech?utm_source=live",
+        styles: {
+            width: "1000px",
+            height: "700px",
+            border: "none",
+            position: "relative"
+        }
+    };
+</script>
+
+<script type="text/javascript" src="/wp-content/uploads/customer-scripts/kalender-iframe.js"></script>
